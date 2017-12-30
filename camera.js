8d1ac2403e3ffee22f8c7147e3d634a4e7989628
noenadies <33671023+noenadies@users.noreply.github.com>
2017-12-30 00:02:17 -0500

    Update camera.js

--- camera.js
+++ camera.js
@@ -20,11 +20,11 @@ var mir;
 
         var IDE_HOOK = false;
         var VERSION = '2.6.2';
- var bmdf1;
+
+  var bmdf1;
   var bmdf3;
   var bmdf2; 
-  var bmd; 
-        
+  var bmd;     
 var game = new Phaser.Game(vw,vh, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
@@ -121,15 +121,7 @@ var  style={ font: "bold "+String(vw*0.04)+"px Arial", fill: "#B21437", boundsAl
  //game.camera.follow(fon, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
 
 
-  bmd = game.make.bitmapData(mundox, mundoy);
-    bmd.addToWorld();
-      bmdf1 = game.make.bitmapData(mundox, mundoy);
-    bmdf1.addToWorld();
 
-      bmdf2 = game.make.bitmapData(mundox, mundoy);
-    bmdf2.addToWorld();
-      bmdf3 = game.make.bitmapData(mundox, mundoy);
-    bmdf3.addToWorld();
 
 editex.update();
 
@@ -167,159 +159,4 @@ function render() {
 
 
 function act_btngraf(){var textoecua0="";
-
-
-  editex.visiblebasico(false);
-if(editex.booltexto_f3){
-
- 
-textoecua=editex.valortexto_f3;
-
-bmdf3.clear();
-
-
-
-
-theplot=[];
-theplot.push(new Fooplot(document.getElementById('theplot')));
-
-
-
-
-theplot[0].addPlot(String(textoecua),FOOPLOT_TYPE_FUNCTION,{'color':$(this).find('.data-color').val()});
-
-  
-   theplot[0].reDraw();
-  colorf3='#F32D09';
-    ecuacion(bmdf3,colorf3);
-
-
 }
-if(editex.booltexto_f2){
-textoecua=editex.valortexto_f2;
-
-
-bmdf2.clear();
-
-
-
-
-theplot=[];
-theplot.push(new Fooplot(document.getElementById('theplot')));
-
-
-
-
-theplot[0].addPlot(String(textoecua),FOOPLOT_TYPE_FUNCTION,{'color':$(this).find('.data-color').val()});
-
-   theplot[0].xmin=-10;
-
-   theplot[0].xmax=10;
-   theplot[0].reDraw();
-  colorf2='#E9FA05';
-    ecuacion(bmdf2,colorf2);
-}
-if(editex.booltexto_f1){
-textoecua=editex.valortexto_f1;
-
-
-
-bmdf1.clear();
-
-
-
-
-theplot=[];
-theplot.push(new Fooplot(document.getElementById('theplot')));
-
-
-
-
-theplot[0].addPlot(String(textoecua),FOOPLOT_TYPE_FUNCTION,{'color':$(this).find('.data-color').val()});
-
-   theplot[0].xmin=-20;
-
-   theplot[0].xmax=-10;
-   theplot[0].reDraw();
-  colorf1='#FA05AE';
-    ecuacion(bmdf1,colorf1);
-
-
-
-
-}
-
- 
-
-}
-
-
-
-
-
-function ecuacion (bmdobj,colr){
-var cc=0;
-
-var  arrayl=[]; 
-
-arrayl=[];
-//arrayl.pop();
-
-
-
-for(var i=1; i<fy1.length;i=i+1){
-
- arrayl.push(new Phaser.Point(fx1[i],fy1[i]));
-}
-
-
-if(editex.booltexto_minmax){
-var indicefy1=0;
-indicefy1=fy1.length/2;
-ox=Math.round(fx1[indicefy1]);
- oy=Math.round(fy1[indicefy1]);
- console.log(ox+"  "+oy);
-//cambiartextx();
-}
-
-
-
-var  n1=0;
-var n2=0;
-var f=unidadmedida;
-var ct=0;
-for(var i=1; i<arrayl.length;i=i+1){
-  ct=ct;
-  n1=ct;
-  n2=ct+1;
- 
-
-
-bmdobj.line(mundox/2+(arrayl[n1].x-ox)*f,mundoy/2-(arrayl[n1].y-oy)*f,
-  mundox/2+(arrayl[n2].x-ox)*f,mundoy/2 -(arrayl[n2].y-oy)*f,colr, 7);
-ct=ct+1;
-
-}
-
-
-
-fy1=[];
-fx1=[];
-
-
-}
-
-
-
-      function btflechad_phaser(visi){
-editex.visiblebasico(visi);
-editex.visibletrigono(!visi);
-//editex.visiblebasico(false);
-      }
-
-
-
-
-
-
-
